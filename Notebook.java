bdf601616bbb3e055ed9e13e030b5b8053ec8950
lucyimhoff <lucyi4@nycstudents.net>
2024-12-12 14:32:32 +0000

    printingNaN

--- Notebook.java
+++ Notebook.java
@@ -3,11 +3,10 @@ public class Notebook{
     private double height;
     private double width;
     public Notebook(int pages, double height, double width){
-        double area = height * width;
     }
-    public String determineCost(double area, int pages){
-        double cost = area/(pages/2);
-        return "the cost of the notebook is: " + cost;
+    public double determineCost(){
+        double cost = (height * width)/(pages/2);
+        return cost;
     }
 
 }
